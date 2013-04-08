b8d6a38bbb8e8b9d0212a3ea1957f5d7e16c509a
Ivo van der Wijk <github@in.m3r.nl>
2013-04-08 17:01:07 +0200

    initial implementation; based on Page; m2m hacks

---
+++ wheelcms_categories/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django import forms
+
+from wheelcms_axle.content import Content
+from wheelcms_spokes.page import PageBase, PageType, PageForm
+from wheelcms_axle.content import type_registry
+
+def fix_form(f, *args, **kwargs):
+    f.fields['categories'] = forms.ModelMultipleChoiceField(queryset=Category.objects.all(), required=False)
+    if 'instance' in kwargs:
+        f.fields['categories'].initial = kwargs['instance'].categories.all()
+
+def save_form(form, instance, commit=True):
+    old_save_m2m = form.save_m2m
+
+    def save_m2m():
+        old_save_m2m()
+        instance.categories.clear()
+        for cat in form.cleaned_data['categories']:
+            instance.categories.add(cat)
+
+    form.save_m2m = save_m2m
+
+class Category(PageBase):
+    items = models.ManyToManyField(Content, related_name="categories")
+    ## manytomany to content
+
+    def __unicode__(self):
+        return self.title
+
+class CategoryForm(PageForm):
+    ## exclude categories?
+
+    class Meta(PageForm.Meta):
+        model = Category
+
+    items = forms.ModelMultipleChoiceField(queryset=Category.objects.all(), required=False)
+
+class CategoryType(PageType):
+    model = Category
+    title = "A category"
+    form = CategoryForm
+
+type_registry.register(CategoryType)
+
