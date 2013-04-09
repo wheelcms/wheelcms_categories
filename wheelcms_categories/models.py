721f95468e5c9ba2791088b6ab63b48334e7a324
Ivo van der Wijk <github@in.m3r.nl>
2013-04-09 14:14:35 +0200

    fully independent, pluggable categories through 'extends'

--- wheelcms_categories/models.py
+++ wheelcms_categories/models.py
@@ -2,24 +2,10 @@ from django.db import models
 from django import forms
 
 from wheelcms_axle.content import Content
+
 from wheelcms_spokes.page import PageBase, PageType, PageForm
 from wheelcms_axle.content import type_registry
 
-def fix_form(f, *args, **kwargs):
-    f.fields['categories'] = forms.ModelMultipleChoiceField(queryset=Category.objects.all(), required=False)
-    if 'instance' in kwargs:
-        f.fields['categories'].initial = kwargs['instance'].categories.all()
-
-def save_form(form, instance, commit=True):
-    old_save_m2m = form.save_m2m
-
-    def save_m2m():
-        old_save_m2m()
-        instance.categories.clear()
-        for cat in form.cleaned_data['categories']:
-            instance.categories.add(cat)
-
-    form.save_m2m = save_m2m
 
 class Category(PageBase):
     items = models.ManyToManyField(Content, related_name="categories")
@@ -34,12 +20,33 @@ class CategoryForm(PageForm):
     class Meta(PageForm.Meta):
         model = Category
 
-    items = forms.ModelMultipleChoiceField(queryset=Category.objects.all(), required=False)
+    items = forms.ModelMultipleChoiceField(queryset=Content.objects.all(),
+                                           required=False)
 
 class CategoryType(PageType):
     model = Category
     title = "A category"
     form = CategoryForm
 
-type_registry.register(CategoryType)
+    @classmethod
+    def extend_form(cls, f, *args, **kwargs):
+        f.fields['categories'] = forms.ModelMultipleChoiceField(
+            queryset=Category.objects.all(), required=False)
+        if 'instance' in kwargs:
+            f.fields['categories'].initial = kwargs['instance'].categories.all()
+        f.advanced_fields += ["categories"]
+
+    @classmethod
+    def extend_save(cls, form, instance, commit=True):
+        old_save_m2m = form.save_m2m
+
+        def save_m2m():
+            old_save_m2m()
+            instance.categories.clear()
+            for cat in form.cleaned_data['categories']:
+                instance.categories.add(cat)
+
+        form.save_m2m = save_m2m
+
+type_registry.register(CategoryType, extends=Content)
 
